b68771a207c86edd678cc622bbbab73309460287
ansgarwenzel <ansgar.wenzel@gmail.com>
2016-10-26 20:53:37 +0200

    Finished package pretty much. next version could have different outputs or different number bases.

--- R/fizzbuzz.R
+++ R/fizzbuzz.R
@@ -1,18 +1,3 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
 #' This function calculates fizzbuzz as is often asked in interviews, thus giving interviewees the possible answer
 #' 'I use fizzbuzzR::fizzbuzz() with standard options'
 #' @param start denotes the start of the for loop
